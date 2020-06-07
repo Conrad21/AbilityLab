c76ef5fcbbbd8fde539a7df37f334163df6b84ac
Conrad21 <ConradPtasznik@gmail.com>
2020-06-06 22:07:52 -0500

    adding data

--- React App/src/Events.jsx
+++ React App/src/Events.jsx
@@ -10,9 +10,6 @@ class Events extends Component {
         chart: {
           id: "basic-bar",
         },
-        xaxis: {
-          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
-        },
       },
       series: [
         {
