36c48b9c7f8d20522bfbebc374b4d9694f636981
Conrad21 <ConradPtasznik@gmail.com>
2020-06-12 19:49:07 -0500

    small changes

--- React App/src/Events.jsx
+++ React App/src/Events.jsx
@@ -1,6 +1,20 @@
 import React, { Component } from "react";
 import Chart from "react-apexcharts";
 
+const data = [
+  [0, 6],
+  [2, 3],
+  [4, 6],
+  [6, 2],
+  [8, 5],
+  [12, 6],
+  [14, 3],
+  [16, 6],
+  [18, 2],
+  [20, 6],
+  [22, 2],
+];
+
 class Events extends Component {
   constructor(props) {
     super(props);
@@ -8,23 +22,31 @@ class Events extends Component {
     this.state = {
       options: {
         chart: {
-          id: "basic-bar",
+          id: "scatter",
+          type: "scatter",
         },
       },
       series: [
         {
           name: "series-1",
-          data: [30, 40, 45, 50, 49, 60, 70, 91],
+          data: data,
         },
       ],
+
       options: {
+        markers: {
+          radius: 112,
+          colors: "#FF0032",
+        },
         chart: {
           height: 350,
           type: "scatter",
           zoom: {
+            enabled: true,
             type: "xy",
           },
         },
+
         dataLabels: {
           enabled: false,
         },
@@ -40,12 +62,12 @@ class Events extends Component {
             },
           },
         },
-        xaxis: {
-          type: "datetime",
-        },
+        // xaxis: {
+        //   type: "datetime",
+        // },
         yaxis: {
-          max: 70,
-          min: -100,
+          max: 10,
+          min: 0,
         },
       },
     };
@@ -59,7 +81,7 @@ class Events extends Component {
             <Chart
               options={this.state.options}
               series={this.state.series}
-              type="bar"
+              type="scatter"
               width="500"
             />
           </div>
